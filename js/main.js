9a857722a6b1b796b5d9c3cf2a603fee2c4d20d1
young <dmsdud4972@gmail.com>
2020-10-30 16:00:01 +0900

    2.2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-
-    $(".scroll").click(function(event){            
-            event.preventDefault();
-            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+$(document).ready(function(){
+       /* one page scroll */
+       var myFullpage = new fullpage('#fullpage', {
+        lazyLoad: true,
+      onLeave: function(origin, destination, direction){
+         var params = { destination:destination };
+         if ( params["destination"]["index"] > 0 ){
+            $("nav").addClass("fix");
+         } else {
+            $("nav").removeClass("fix");
+         }
+      }
     });
+    
 });
