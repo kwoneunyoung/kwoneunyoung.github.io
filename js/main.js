b9c8ddd2ffd74d46ac4b62c04713515bb5769a3a
young <dmsdud4972@gmail.com>
2020-11-05 18:24:19 +0900

    6

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function(){    
 
-    $('.accordion-list > li > .answer first').slideUp();
-      
-    $('.accordion-list > li > .answer').hide();
-      
+  $(".accordion-list > li.active .answer").slideDown();
+ 
+    
     $('.accordion-list > li').click(function() {
       if ($(this).hasClass("active")) {
         $(this).removeClass("active").find(".answer").slideUp();
-      } else {
-        $(".accordion-list > li.active .answer").slideUp();
+      } else if ($(".accordion-list > li.active .answer").slideUp()){
+        
         $(".accordion-list > li.active").removeClass("active");
         $(this).addClass("active").find(".answer").slideDown();
       }
       return false;
     });
     
+
+   
+   
+
   });
