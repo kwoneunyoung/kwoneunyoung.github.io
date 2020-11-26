740504ca32a060b89d3c2958e65a051f4b90df2a
young <dmsdud4972@naver.com>
2020-11-27 00:06:10 +0900

    11

--- javaPractice/Hoa/src/hoaData/fileRead.java
+++ javaPractice/Hoa/src/hoaData/fileRead.java
@@ -21,6 +21,7 @@ public class fileRead {
 
             ArrayList<hoaVO> hoaVOList = new ArrayList<hoaVO>();
             ArrayList<hoaVO> tempList = new ArrayList<hoaVO>(); //임시리스트
+        	
             ArrayList<hoaVO> tempPriceList = new ArrayList<hoaVO>();
             
             String line = "";
@@ -33,57 +34,39 @@ public class fileRead {
                
                String price = str.substring(26,35);
                int price2=Integer.parseInt(price);
-               //String price3 = Integer.toString(price2);
                
                String amount = str.substring(35,47);
                int amount2=Integer.parseInt(amount);
-               //String amount3 = Integer.toString(amount2);
-               
-               //System.out.println("시간 : " + time +" 종목코드 :" + nameCode + " 일련번호 : " + serialNumber + " 가격 : " + price2 +" 수량 : " + amount2);
                
                //String였던 시간으로 형변화
                String day = null;
                String day30 = null;
                SimpleDateFormat transFormat = new SimpleDateFormat("HH:mm:ss"); //뒤에 S붙이면 밀리세컨드까지 출력 가능
                Date to = transFormat.parse(time);
-               
-               //파일 시간 --Thu Jan 01 14:50:13 KST 1970
-               //System.out.println(to);
-               
+    
                Calendar cal = Calendar.getInstance();
                cal.setTime(to);
                day = transFormat.format(cal.getTime());
-               
-               //30분 더하기
-               //cal.add(Calendar.MINUTE, 30);
-               //day30=transFormat.format(cal.getTime());
-               //System.out.println("30분씩" + day30);
-               
+           
                hoaVO hoaVO1 = new hoaVO();
                hoaVO1.setNameCode(nameCode);
                hoaVO1.setTime(day);
-               hoaVO1.setSerialNumber(serialNumber);
                hoaVO1.setPrice(price);
-               hoaVO1.setAmount(amount);
                hoaVOList.add(hoaVO1);
             }
             
-            Collections.sort(tempList, new hoaVO()); //종목별 정렬
             for(int i=0; i<hoaVOList.size()-1; i++) {//파일을 다 읽어서 만든 리스트만큼 반복문을 돌림
-            	
                   if(hoaVOList.get(i).getNameCode().equals(hoaVOList.get(i+1).getNameCode())) {
                 	tempList.add(hoaVOList.get(i));
                 	} else {
                 	  tempList.add(hoaVOList.get(i));
-                  } //리스트에 담긴 상태
+                  } //리스트에 담긴 상태(total값이 안 들어있음)
+                  
                   //System.out.println(tempList.get(i));
             } 
             
-            
-            for(int i=0; i<tempList.size()-1; i++) {
-	            	Collections.sort(tempList, new hoaVO()); //시간별 정렬 
+            for(int i=0; i<hoaVOList.size()-1; i++) {
 	            	
-	            	hoaVO hoaVO1 = new hoaVO();
 	            	String even=null;
 	            	String start = tempList.get(i).getTime();
 	            	
@@ -95,35 +78,29 @@ public class fileRead {
 	            	even = dateFormat.format(cal.getTime());
 	            	
 		            	if(cal.get(Calendar.MINUTE) <=30) { //시간(분)이 30분 보다 적거나 같을 때
-		            		cal.set(Calendar.MINUTE, 00);
-		            		hoaVO1.setTotal(dateFormat.format(cal.getTime()));
-		            		hoaVOList.add(hoaVO1);
-		            		System.out.println(hoaVO1);
-		            		//System.out.println(dateFormat.format(cal.getTime()));
+		            		cal.set(Calendar.MINUTE, 00); //모든 분을 00으로 지정
 		            		
+		            		hoaVOList.get(i).setTotal(dateFormat.format(cal.getTime()));
+		            		
+		            
 		            	} else { //30분을 벗어난 시간
 		            		cal.set(Calendar.MINUTE, 30);
-		            		hoaVO1.setTotal(dateFormat.format(cal.getTime()));
-		            		hoaVOList.add(hoaVO1);
-		            		//System.out.println(dateFormat.format(cal.getTime()));
+		            	
+		            		hoaVOList.get(i+1).setTotal(dateFormat.format(cal.getTime()));
 		            	}
-		            		System.out.println(hoaVO1.getTotal());
-		            	}
-	         
-            	
-            	//System.out.println(tempList.get(i).getNameCode() + " "+even + " " +tempList.get(i).getPrice()); //시간 오름 차순 정렬 되어있는 상태
-//            	System.out.println(even);
-//            	System.out.println(cal.get(Calendar.HOUR_OF_DAY)); //시간 가져왔음
-//            	System.out.println(cal.get(Calendar.MINUTE)); //분 가져왔음
-//            	System.out.println(cal.get(Calendar.SECOND)); //분 가져왔음
-            	
-           
+            }
             
-           int firstPrice; //처음 거래 가격
-           int lastPrice; //마지막 거래 가격
-           int lowPirce; //최저가
-           int highPrice; //최고가
-        
+            
+            Collections.sort(hoaVOList, new hoaVO()); //종목 > 새 시간 > 가격
+           
+           
+            for(int i=0; i<hoaVOList.size()-1; i++) {
+            	System.out.println(hoaVOList.get(i));
+            	 //System.out.println(hoaVOList.get(i).getTotal());
+                 
+            }
+ 	
+
             
         } catch (Exception e) {
            e.printStackTrace();
--- javaPractice/Hoa/src/hoaData/hoaVO.java
+++ javaPractice/Hoa/src/hoaData/hoaVO.java
@@ -5,13 +5,13 @@ import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
 
-public class hoaVO implements Comparator <hoaVO>{
+public class hoaVO implements Comparator <hoaVO> {
 
 	private String nameCode;
 	private String time;
-	private String serialNumber;
+	//private String serialNumber;
 	private String price;
-	private String amount;
+	//private String amount;
 	private String total;
 
 	public String getTotal() {
@@ -32,38 +32,52 @@ public class hoaVO implements Comparator <hoaVO>{
 	public void setNameCode(String nameCode) {
 		this.nameCode = nameCode;
 	}
-	public String getSerialNumber() {
-		return serialNumber;
-	}
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
-	}
+
 	public String getPrice() {
 		return price;
 	}
 	public void setPrice(String price) {
 		this.price = price;
 	}
-	public String getAmount() {
-		return amount;
-	}
-	public void setAmount(String amount) {
-		this.amount = amount;
+
+	
+//	@Override
+//	public int compare(hoaVO o1, hoaVO o2) {
+//		
+//		return o1.getTime().compareTo(o2.getTime());
+//	}
+
+
+
+	
+	@Override
+	public int compare(hoaVO o1, hoaVO o2) {
+		int ret =0;
+	
+		if(o1.nameCode == o2.nameCode) { //종목코드가 같으면
+			if(o1.total.compareTo(o2.total)==0) { //시간이 같으면
+				
+			} else if(o1.total.compareTo(o2.total) < 0) {
+				ret = -1;
+			} else if(o1.total.compareTo(o2.total) > 0) {
+				ret= 1;
+			}
+
+			if(o1.nameCode != o2.nameCode) { //종목코드가 다르면 오름차순
+				ret = -1;
+			}
+			
+		}
+		return ret;
 	}
 	@Override
 	public String toString() {
-		return "hoaVO [nameCode=" + nameCode + ", time=" + time + ", serialNumber=" + serialNumber + ", price=" + price
-				+ ", amount=" + amount + ", total=" + total + "]";
+		return "hoaVO [nameCode=" + nameCode + ", price=" + price + ", total=" + total + "]";
 	}
-
-	@Override
-	public int compare(hoaVO o1, hoaVO o2) {
-		
-		return o1.getTime().compareTo(o2.getTime());
-	}
-
-
-
+//	@Override
+//	public String toString() {
+//		return "hoaVO [nameCode=" + nameCode + ", time=" + time + ", price=" + price + ", total=" + total + "]";
+//	}
 
 	
 }
